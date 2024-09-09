bed1827c3112f238cc873100467021a3d7377b1c
eroepke <164393462+ethroepke@users.noreply.github.com>
2024-09-09 14:23:09 -0500

    exp3

--- Experiments/eroepke/exp3/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Experiments/eroepke/exp3/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -21,6 +21,6 @@ public class MainActivity extends AppCompatActivity {
 
         /* initialize UI elements */
         messageText = findViewById(R.id.main_msg_txt);      // link to message textview in the Main activity XML
-        messageText.setText("Hello World");
+        messageText.setText("This is a test for push/commit in Android Studio");
     }
 }
