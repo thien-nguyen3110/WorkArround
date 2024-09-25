89be52cab03b73712f09c537e2b6bc5cc8dcb1b9
eroepke <164393462+ethroepke@users.noreply.github.com>
2024-09-25 12:34:29 -0500

    login page and removed unecessary files

---
+++ Frontend/loginPage/app/src/main/java/com/example/androidexample/loginActivity.java
@@ -0,0 +1,33 @@
+package com.example.androidexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import org.w3c.dom.Text;
+import 
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreeate(Bundle savedInstanceState){
+        //
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Initialize UI Elements
+        Button signInButton = findViewById(R.id.submitButton);
+
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, );
+                startActivity(intent);
+            }
+        });
+    }
+}
