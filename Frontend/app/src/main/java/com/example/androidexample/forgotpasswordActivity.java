af83a38e1f2bd7fbba1016525f738ddded376694
Ali Mannan <amannan@iastate.edu>
2024-09-30 17:24:10 -0500

    Screen for forgot password

--- Frontend/app/src/main/java/com/example/androidexample/forgotpasswordActivity.java
+++ Frontend/app/src/main/java/com/example/androidexample/forgotpasswordActivity.java
@@ -4,14 +4,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class forgotpasswordActivity extends AppCompatActivity {
 
+    private Button back_button;
+    private EditText email_input;
+    private Button submit_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.forgotpassword);
+
+
+
     }
+
+
+
+
 }
