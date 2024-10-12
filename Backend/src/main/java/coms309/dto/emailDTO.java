f535a496c0193d3b3e6f0c500cbc6af7b47a0bee
eroepke <164393462+ethroepke@users.noreply.github.com>
2024-10-12 11:51:08 -0500

    dto

--- Backend/src/main/java/coms309/dto/emailDTO.java
+++ Backend/src/main/java/coms309/dto/emailDTO.java
@@ -6,12 +6,4 @@ import org.springframework.lang.Nullable;
 public class EmailDTO {
     private String email;
 
-    // Getter and Setter
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
