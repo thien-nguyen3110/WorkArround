4bc1804fecf7f38e4311c67b2ebcf9e60131e9fe
eroepke <164393462+ethroepke@users.noreply.github.com>
2024-10-12 09:56:52 -0500

    login request

--- Frontend/app/src/main/java/com/example/androidexample/loginActivity.java
+++ Frontend/app/src/main/java/com/example/androidexample/loginActivity.java
@@ -119,22 +119,16 @@ public class loginActivity extends AppCompatActivity {
     }
 
     public void loginRequest() {
-        String url = "http://coms-3090-046.class.las.iastate.edu:8080/api/userprofile/login";
-        JSONObject loginData = new JSONObject();
+        // Construct the URL with query parameters for username and password
+        String url = "http://coms-3090-046.class.las.iastate.edu:8080/api/userprofile/login?username="
+                + usernameInput.getText().toString()
+                + "&password=" + passwordInput.getText().toString();
 
-        try {
-            loginData.put("username", usernameInput.getText().toString());
-            loginData.put("password", passwordInput.getText().toString());
-            Log.i("hidden", loginData.toString());
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        // Creating a custom request to allow for GET requests with a body
+        // Use GET request without body
         JsonObjectRequest loginRequest = new JsonObjectRequest(
                 Request.Method.GET,
                 url,
-                null, // Pass null for body since we can't send a body in GET
+                null,  // No body is sent with a GET request
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
@@ -151,35 +145,10 @@ public class loginActivity extends AppCompatActivity {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         Log.e("Login Error", error.toString());
-                        messageText.setText(error.toString());
+                        messageText.setText("Invalid credentials, try again.");
                     }
-<<<<<<< HEAD
-                }) {
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<>();
-                params.put("username", usernameInput.getText().toString());
-                params.put("password", passwordInput.getText().toString());
-                return params; // This is where parameters get sent
-            }
-        };
-=======
                 }
-                )
-                {
-                    @Override
-                    public Map<String, String> getHeaders() {
-                        Map<String, String> headers = new HashMap<>();
-                        return headers;
-                    }
-
-                    @Override
-                    protected Map<String, String> getParams() {
-                        Map<String, String> params = new HashMap<>();
-                        return params;
-                    }
-                };
->>>>>>> e5441b70751d6a8d23521143890aad1fa74d10f0
+        );
 
         Volley.newRequestQueue(this).add(loginRequest);
     }
