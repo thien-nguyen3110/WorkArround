a79024bb4abd74998972748edca5e0d895ae7b7f
eroepke <164393462+ethroepke@users.noreply.github.com>
2024-10-14 14:29:10 -0500

    updates

--- Backend/src/main/java/coms309/dto/payDetailsDTO.java
+++
@@ -1,9 +0,0 @@
-package coms309.dto;
-
-public class PayDetailsDTO {
-    private double hoursWorked;
-    private double payRate;
-    private double bonusPay;
-    private double deductibles;
-
-}
