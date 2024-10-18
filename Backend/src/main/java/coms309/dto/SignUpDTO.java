5aa9b6741c1705c48725db65217d033bfda15d5a
Mubassir Serneabat Sudipto <msudipto@iastate.edu>
2024-10-18 10:53:29 -0500

    Added new controllers, entities, and service files for login. Removed unused files and functions.

---
+++ Backend/src/main/java/coms309/dto/SignUpDTO.java
@@ -0,0 +1,60 @@
+
+package coms309.dto;
+
+public class SignUpDTO {
+    private String name;
+    private String email;
+    private String username;
+    private String password;
+    private String verifyPassword;
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public String getEmail() {
+
+        return email;
+    }
+
+    public void setEmail(String email) {
+
+        this.email = email;
+    }
+
+    public String getUsername() {
+
+        return username;
+    }
+
+    public void setUsername(String username) {
+
+        this.username = username;
+    }
+
+    public String getPassword() {
+
+        return password;
+    }
+
+    public void setPassword(String password) {
+
+        this.password = password;
+    }
+
+    public String getVerifyPassword() {
+
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+
+        this.verifyPassword = verifyPassword;
+    }
+}
