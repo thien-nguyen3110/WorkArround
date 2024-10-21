45fb87a4f1b0671064f6862c5a871c383088e7e5
eroepke <164393462+ethroepke@users.noreply.github.com>
2024-10-21 14:51:58 -0500

    signup

--- Frontend/app/src/main/java/com/example/androidexample/joinNowActivity.java
+++ Frontend/app/src/main/java/com/example/androidexample/joinNowActivity.java
@@ -227,9 +227,7 @@ public class joinNowActivity extends AppCompatActivity {
                     signup_details.put("username", usernameFilled);
                     signup_details.put("email", emailFilled);
                     signup_details.put("password", passwordFilled);
-
                     signup_details.put("name", nameFilled);
-                    signup_details.put("verifyPassword", passwordFilled);
 
                 } catch (JSONException e) {
                     throw new RuntimeException(e);
