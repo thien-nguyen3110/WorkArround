ab56b770a1bc8a4dc7e176a770d7e2a16f4b436b
eroepke <164393462+ethroepke@users.noreply.github.com>
2024-10-23 14:24:38 -0500

    employer

--- Frontend/app/src/main/java/com/example/androidexample/employerActivity.java
+++ Frontend/app/src/main/java/com/example/androidexample/employerActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -22,4 +23,19 @@ import java.util.concurrent.TimeUnit;
 
 public class employerActivity extends AppCompatActivity {
 
+    private Button checkInOut;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.employer);
+
+        checkInOut = findViewById(R.id.checkButton);
+
+        //Set colors
+        int checkGrey = Color.rgb(211, 211, 211);
+        //Change colors to grey for buttons
+        checkInOut.setBackgroundColor(checkGrey);
+    }
 }
