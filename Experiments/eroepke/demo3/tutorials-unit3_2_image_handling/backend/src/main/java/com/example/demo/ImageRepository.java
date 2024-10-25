75709a52d3b035b28c7e6be1c56a578cc991ba42
eroepke <164393462+ethroepke@users.noreply.github.com>
2024-10-25 13:34:33 -0500

    remove

--- Experiments/eroepke/demo3/tutorials-unit3_2_image_handling/backend/src/main/java/com/example/demo/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-    Image findById(int id);
-}
