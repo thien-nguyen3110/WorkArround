a212fb7934c3a0e4ebc420c8d2cb9deb729beae8
Mubassir Serneabat Sudipto <msudipto@iastate.edu>
2024-10-25 22:49:01 -0500

    Modified and updated files for ws_3_1_one_websocket

--- Experiments/msudipto/demo3/WebSocket/3_1_one_websocket/Client_JS/websocket.js
+++ Experiments/msudipto/demo3/WebSocket/3_1_one_websocket/Client_JS/websocket.js
@@ -1,43 +1,28 @@
-
 var ws;
 
 function connect() {
     var username = document.getElementById("username").value;
-    var token = document.getElementById("token").value;
     var wsserver = document.getElementById("wsserver").value;
-    var url = wsserver + username + "/" + token;
+    var url = wsserver + username;
+    //var url = "ws://echo.websocket.org";
 
     ws = new WebSocket(url);
 
-    ws.onopen = function(event) { // Connection opened
+    ws.onmessage = function(event) { // Called when client receives a message from the server
+        console.log(event.data);
+
+        // display on browser
+        var log = document.getElementById("log");
+        log.innerHTML += "message from server: " + event.data + "\n";
+    };
+
+    ws.onopen = function(event) { // called when connection is opened
         var log = document.getElementById("log");
         log.innerHTML += "Connected to " + event.currentTarget.url + "\n";
     };
-
-    ws.onmessage = function(event) { // Message received
-        console.log(event.data);
-        var log = document.getElementById("log");
-        log.innerHTML += "Server: " + event.data + "\n";
-    };
-
-    ws.onerror = function(event) { // Connection error
-        var log = document.getElementById("log");
-        log.innerHTML += "Error: " + event.message + "\n";
-    };
-
-    ws.onclose = function(event) { // Connection closed
-        var log = document.getElementById("log");
-        log.innerHTML += "Disconnected from server.\n";
-    };
 }
 
-function send() {
+function send() {  // this is how to send messages
     var content = document.getElementById("msg").value;
-    if (ws && ws.readyState === WebSocket.OPEN) {
-        ws.send(content);
-        document.getElementById("msg").value = ""; // Clear input after sending
-    } else {
-        var log = document.getElementById("log");
-        log.innerHTML += "Unable to send message. Not connected.\n";
-    }
+    ws.send(content);
 }
