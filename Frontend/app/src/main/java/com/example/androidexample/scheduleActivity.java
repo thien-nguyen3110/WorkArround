4675fd80b85f29dbd4488a5e3dbd0ae2c933259e
Ali Mannan <amannan@iastate.edu>
2024-11-03 18:20:14 -0600

    added calendar

--- Frontend/app/src/main/java/com/example/androidexample/scheduleActivity.java
+++ Frontend/app/src/main/java/com/example/androidexample/scheduleActivity.java
@@ -25,4 +25,10 @@ import org.json.JSONObject;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class scheduleActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.assignproject);
+    }
+
 }
